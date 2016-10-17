b19d27fbe4c91adcd215e28dc534a9c48a33816f
Fernando de Paz <fdepaz@uci.edu>
2016-10-17 15:44:57 -0700

    install script
    added install script for 1st time Run

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo pip install google-api-python-client
+sudo apt-get install tkdnd
+sudo apt-get install python-pip python-dev ipython
+sudo apt-get install bluetooth libbluetooth-dev
+sudo pip install pybluez
